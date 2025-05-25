a1272d9241c27b8f0426603df7bf631eff73e5d1
Mellamomax <max.skold@hotmail.com>
2025-05-25 20:47:33 +0200

    2025 fix

--- custom_components/ica_shopping_list/__init__.py
+++ custom_components/ica_shopping_list/__init__.py
@@ -13,7 +13,7 @@ from homeassistant.components import http
 from homeassistant.components.http.data_validator import RequestDataValidator
 from homeassistant.helpers import intent
 import homeassistant.helpers.config_validation as cv
-from homeassistant.util.json import load_json, save_json
+from homeassistant.helpers.json import load_json, save_json
 from homeassistant.components import websocket_api
 from homeassistant.const import (CONF_PASSWORD, CONF_USERNAME)
 
